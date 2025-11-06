41c0bd432314179707ff113bf3f6c1d6d66f93e7
Vojtěch Kulvait <kulvait@gmail.com>
2025-11-06 15:44:42 +0100

    Build library with ESNext

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 // --- Vanilla/D3 exports (keep original functionality)
-export {TransferFunctionEditor} from './core/TransferFunctionEditor';
-export {ColorMapEditor} from './core/ColorMapEditor';
-export {ColorPicker} from './core/ColorPicker';
-export {TransparencyEditor} from './core/TransparencyEditor';
-export * from './core/convert';
-export * from './core/Types';
+export {TransferFunctionEditor} from './core/TransferFunctionEditor.js';
+export {ColorMapEditor} from './core/ColorMapEditor.js';
+export {ColorPicker} from './core/ColorPicker.js';
+export {TransparencyEditor} from './core/TransparencyEditor.js';
+
+// For utility files / types, also add .js
+export * from './core/convert.js';
+export * from './core/Types.js';
